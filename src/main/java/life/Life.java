91bcfe40fbdfae11f478374259b3f674ecd7b585
Phil Goodwin <phil.goodwin@gmail.com>
2020-09-11 20:25:21 -0700

    Refactor to allow for larger fields

--- src/main/java/life/Life.java
+++ src/main/java/life/Life.java
@@ -8,52 +8,59 @@ class Life {
     static private char ACTIVE = 'X';
     static private char INACTIVE = '_';
     static public char empty[][] = {
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
     static public char centerActive[][] = {
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, ACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, ACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
 
     static public char topLeftActive[][] = {
-            { ACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {ACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
 
     static public char topRightActive[][] = {
-            { INACTIVE, INACTIVE, ACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {INACTIVE, INACTIVE, ACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
 
     static public char example2[][] = {
-            { ACTIVE, ACTIVE, INACTIVE},
-            { INACTIVE, ACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {ACTIVE, ACTIVE, INACTIVE},
+            {INACTIVE, ACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
 
     static public char example3[][] = {
-            { ACTIVE, ACTIVE, ACTIVE},
-            { INACTIVE, ACTIVE, INACTIVE},
-            { INACTIVE, ACTIVE, INACTIVE}
+            {ACTIVE, ACTIVE, ACTIVE},
+            {INACTIVE, ACTIVE, INACTIVE},
+            {INACTIVE, ACTIVE, INACTIVE}
     };
 
     static public char example4[][] = {
-            { ACTIVE, ACTIVE, ACTIVE},
-            { INACTIVE, ACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {ACTIVE, ACTIVE, ACTIVE},
+            {INACTIVE, ACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
 
     private char cells[][];
     private char nextCells[][] = {
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE},
-            { INACTIVE, INACTIVE, INACTIVE}
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE},
+            {INACTIVE, INACTIVE, INACTIVE}
     };
+
+    public Life() {
+    }
+
+    public Life(char[][] template) {
+        cells = template;
+    }
 
     public static void main(String args[]) {
         run(topLeftActive);
@@ -94,12 +101,12 @@ class Life {
                 {INACTIVE, INACTIVE, INACTIVE}
         };
 
-        for (int x=0; x < 3; x++) {
-            for (int y=0; y < 3; y++) {
+        for (int x = 0; x < initialCells.length; x++) {
+            for (int y = 0; y < initialCells[x].length; y++) {
                 applyRules(new Cell(x, y));
             }
         }
-        
+
         return nextCells;
     }
 
@@ -118,19 +125,14 @@ class Life {
 
     private int activeNeighborCount(Cell cell) {
         int active = 0;
-        List<Cell> neighbors = neighborOf(cell);
+        List<Cell> neighbors = neighborsOf(cell);
 
         for (Cell neighbor : neighbors) {
             if (isActive(neighbor)) {
                 active++;
             }
-
         }
         return active;
-    }
-
-    private void becomeInactive(Cell cell) {
-        nextCells[cell.x][cell.y] = INACTIVE;
     }
 
     private void becomeActive(Cell cell) {
@@ -141,33 +143,21 @@ class Life {
         return cells[cell.x][cell.y] == ACTIVE;
     }
 
-    List<Cell> neighborOf(Cell cell) {
-        List<Cell> neighbor = new ArrayList<>();
-        for (int x=0; x < 3; x++) {
-            for (int y=0; y < 3; y++) {
-                int xDistance = cell.x - x;
-                int yDistance = cell.y - y;
+    List<Cell> neighborsOf(Cell cell) {
+        List<Cell> neighbors = new ArrayList<>();
 
-                if (xDistance == 0 && yDistance == 0) {
-                    continue;
-                }
-
-                if (!(Math.abs(xDistance) > 1 || Math.abs(yDistance) > 1))  {
-                    neighbor.add(new Cell(x, y));
+        for (int x = cell.x - 1; x <= cell.x + 1; x++) {
+            for (int y = cell.y - 1; y <= cell.y + 1; y++) {
+                if (x >= 0 && y >= 0 &&
+                        x < cells.length &&
+                        y < cells[x].length &&
+                        !(x == cell.x && y == cell.y)) {
+                    neighbors.add(new Cell(x, y));
                 }
             }
         }
 
-        return neighbor;
-    }
-
-    private void print() {
-        for (char[] row : cells) {
-            for (char cell : row) {
-                System.out.print(cell);
-            }
-            System.out.println();
-        }
+        return neighbors;
     }
 }
 
--- src/test/java/life/LifeTest.java
+++ src/test/java/life/LifeTest.java
@@ -8,12 +8,17 @@ import static org.hamcrest.Matchers.*;
 class LifeTest {
 
     @Test
-    void neighborOf_shouldReturnOnlyRealNeighbors() {
-        var cells = new Life().neighborOf(new Cell(0, 0));
+    void neighborsOf_shouldReturnOnlyRealNeighbors() {
+        Life life = new Life(Life.empty);
+        var cells = life.neighborsOf(new Cell(0, 0));
 
         assertThat(cells.size(), is(3));
         assertThat(cells, hasItem(new Cell(0, 1)));
         assertThat(cells, hasItem(new Cell(1, 1)));
         assertThat(cells, hasItem(new Cell(1, 0)));
+
+        assertThat(life.neighborsOf(new Cell(1, 0)).size(), is(5));
+        assertThat(life.neighborsOf(new Cell(1, 1)).size(), is(8));
+        assertThat(life.neighborsOf(new Cell(2, 2)).size(), is(3));
     }
 }
